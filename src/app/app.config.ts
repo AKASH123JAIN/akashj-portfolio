d1672fb1e0a589c752d6d72c5c726f58de5e0542
AKASH123JAIN <akash.soni58@gmail.com>
2025-10-16 22:07:27 -0400

    Fixing 404 issues Attepmt #2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,15 @@
 // src/app/app.config.ts
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; // We'll create this next
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideClientHydration(), provideAnimationsAsync()
+    provideRouter(routes, withHashLocation()),
+    provideClientHydration(),
+    provideAnimationsAsync()
   ]
 };
